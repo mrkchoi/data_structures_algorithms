7fb508efefac432885be21d0f913c0b6884d1a4a
Kenneth Choi <kennethichoi@gmail.com>
2019-07-22 21:03:14 -0700

    Implement stack + queue in js

--- stacks_queues/stacks_queues.js
+++ stacks_queues/stacks_queues.js
@@ -34,12 +34,13 @@ class Stack {
       return null;
     }
     let temp = this.top;
-    if (!this.bottom) {
-      return null;
+    if (this.top === this.bottom) {
+      this.bottom = null;
     }
 
     this.top = this.top.next;
     this.length -= 1;
+
     return temp.value;
   }
 
@@ -61,14 +62,61 @@ class Stack {
 
 
 
-
-
-
-
-
-
-
-
-
-
 // Implement a queue
+
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    // if empty
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      // if not empty
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    return this.length += 1;
+  }
+
+  dequeue() {
+    if (!this.front) {
+      return null;
+    }
+    let temp = this.front;
+    if (this.front === this.back) {
+      this.back = null;
+    }
+    this.front = this.front.next;
+    this.length -= 1;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
+}
+
+
+
+
+
+
+
+
+
+
