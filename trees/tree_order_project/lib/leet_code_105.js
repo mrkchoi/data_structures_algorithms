7f50e10b8177ee809d3bd045d92c5742f7745ac1
Kenneth Choi <kennethichoi@gmail.com>
2019-07-24 09:48:00 -0700

    Implement lc 105 (js) build binary tree from preorder + inorder

--- trees/tree_order_project/lib/leet_code_105.js
+++ trees/tree_order_project/lib/leet_code_105.js
@@ -3,7 +3,23 @@
 
 const { TreeNode } = require('./tree_node.js');
 
+// class TreeNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
-function buildTree(preorder, inorder) {
+const buildTree = function(preorder, inorder) {
+  if (!inorder.length) return null;
 
-}
+  let val = preorder.shift();
+  let node = new TreeNode(val);
+  let idx = inorder.indexOf(val);
+
+  node.left = buildTree(preorder, inorder.slice(0, idx));
+  node.right = buildTree(preorder, inorder.slice(idx + 1));
+
+  return node;
+};
