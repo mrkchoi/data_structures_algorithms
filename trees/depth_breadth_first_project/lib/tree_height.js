f83a16d273a92a262e2735925dd3be5faa06606b
Kenneth Choi <kennethichoi@gmail.com>
2019-07-24 10:43:06 -0700

    Implement maximum height of binary tree

--- trees/depth_breadth_first_project/lib/tree_height.js
+++ trees/depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,14 @@
 function treeHeight(root) {
-
+    if (!root) return -1;
+    let height = 0;
+    let queue = [root];
+    while (queue.length) {
+        let node = queue.shift();
+        if (node.left || node.right) height += 1;
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return height;
 }
 
 
