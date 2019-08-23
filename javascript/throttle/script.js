98fa6d8e1ec7b4a49260eead2024a165d9360f69
Kenneth Choi <kennethichoi@gmail.com>
2019-08-22 21:58:28 -0700

    Implement throttle

---
+++ javascript/throttle/script.js
@@ -0,0 +1,37 @@
+
+
+// init throttle HOF with callback fn + limit
+const throttledHandleClick = throttle(getData, 1000);
+// setup event listener on input
+const submit = document.querySelector('#button');
+submit.addEventListener('click', throttledHandleClick);
+// write callback fn
+let counter = 1;
+function getData() {
+  console.log(`getting data`, counter++);
+}
+
+// write throttle function
+function throttle(func, limit) {
+  let lastRan;
+  let lastCall;
+
+  return function() {
+    let context = this;
+    let args = arguments;
+
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastCall);
+      lastCall = setTimeout(() => {
+        // if limit has been reached, execute function and update lastRan
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    } 
+  }
+}
