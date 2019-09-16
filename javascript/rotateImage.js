95ba987405f73b335553e0ed7a9335ffba7faa21
Kenneth Choi <kennethichoi@gmail.com>
2019-09-16 09:40:53 -0700

    Implement in-place rotateImage

--- javascript/rotateImage.js
+++ javascript/rotateImage.js
@@ -21,10 +21,80 @@
 //   [9,6,3]
 // ]
 
-// error check: valid array, .length === [0].length, length === 1
+// Example 2:
 
-// loop from i,j = [0,0],
-// save temp
-// set matrix[i][arr.length - 1]
+// Given input matrix =
+// [
+//   [ 5, 1, 9,11],
+//   [ 2, 4, 8,10],
+//   [13, 3, 6, 7],
+//   [15,14,12,16]
+// ], 
 
-var rotate = function(matrix) {};
+// rotate the input matrix in-place such that it becomes:
+// [
+//   [15,13, 2, 5],
+//   [14, 3, 4, 1],
+//   [12, 6, 8, 9],
+//   [16, 7,10,11]
+// ]
+
+//     0 1 2
+//  0 [1,2,3],
+//  1 [4,5,6],
+//  2 [7,8,9]
+
+//     0 1 2
+//  0 [7,4,1],
+//  1 [8,5,2],
+//  2 [9,6,3]
+
+
+// error check: non-array/matrix, empty matrix, matrix.length === 1
+
+// iterate through matrix, flip on diagonal, then flip on vertical axis
+// for diagonal, only need to flip items up to diagonal line,
+// for vertical flip, only need to flip items up to vertical midpoint
+
+
+function rotate(matrix) {
+  if (!Array.isArray(matrix)) throw new Error("Invalid input: input type must be array");
+  if (matrix.length === 1) return matrix;
+
+  // diagonal flip
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = i; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  // horizontal flip
+  for (let i = 0; i < matrix.length; i++) {
+    let midIdx = Math.floor(matrix[0].length / 2);
+    for (let j = 0; j < midIdx; j++) {
+      [matrix[i][j], matrix[i][matrix.length - 1 - j]] = [
+        matrix[i][matrix.length - 1 - j],
+        matrix[i][j]
+      ];
+    }
+  }
+
+  return matrix;
+};
+
+
+let m1 = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+let m2 = [
+  [1,2,3,4],
+  [5,6,7,8],
+  [9,10,11,12],
+  [13,14,15,16]
+];
+
+rotate(m1);
+// rotate(m2);
