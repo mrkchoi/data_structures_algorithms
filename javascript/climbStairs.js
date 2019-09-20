622d61ffbbe68e9126b067512e9a7d1863d33692
Kenneth Choi <kennethichoi@gmail.com>
2019-09-19 20:49:17 -1000

    Implement climbStairs

---
+++ javascript/climbStairs.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// version #1
+// helper function to climbStep,
+// create closure around count = 0;
+// inside helper function, take either one or two steps,
+// if one step + count <= n, increment step and recursively call step again with new count
+// if two step + count <= n, increment step by 2 and recursively call step again with new count
+// keep track of total combinations that reach n,
+// make sure to increment total each time step === n
+// return total
+
+var climbStairs = function(n) {
+  let totalCount = 0;
+
+  function climb(currentStep, n) {
+    if (currentStep === n) totalCount += 1;
+    if (currentStep > n) return;
+
+    if (currentStep + 1 <= n) {
+      climb(currentStep + 1, n);
+    }
+
+    if (currentStep + 2 <= n) {
+      climb(currentStep + 2, n);
+    }
+  }
+
+  climb(0,n);
+  return totalCount;
+};
+
+// // version #2:
+
+// // we know that if n === 0, we have 0 ways to step
+// // if n === 1, we have exactly one way to step
+// // if n === 2, we have two ways to step,
+// // if we check the pattern moving forward, we can see that the current ways is the sum of the previous two smaller number's ways (ie step[3] = step[1] + step[2]).
+// // we init our base cases
+// // we can store the values for each step inside of an array,
+// // iterate through from idx 2 up to 'n',
+// // at each iteration, set the value in the array at the given index to be the sum of the previous two steps
+// // continue until we reach n,
+// // then, we know that the number of ways available to us is stored in the array at idx n - 1
+// // return array[n - 1];
+
+function climbStairs(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let combos = [0,1,2];
+
+  for (let i = 3; i <= n; i++) {
+    combos[i] = combos[i - 1] + combos[i - 2];
+  }
+
+  return combos[n];
+}
